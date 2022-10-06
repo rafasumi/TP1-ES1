72cc9fe5037c74ac9090245bb8b6aee6313eacaf
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2022-10-06 11:15:35 -0300

    Adicionando middleware de body parser

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
