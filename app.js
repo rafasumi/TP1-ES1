2866967c170e53206aedab108a46b98754f4144c
Isis <isisferreiracarvalho@yahoo.com.br>
2022-10-20 00:29:13 -0300

    merge das modificações do rafa (não sei usar git)

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ app.use(express.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const albums = await Album.findAll({limit: 4});
+  const artists = await Artist.findAll({limit: 4});
+  res.render('index', {albums, artists});
 });
 
 const artistRouter = require('./controllers/artistController');
@@ -18,6 +20,8 @@ const albumRouter = require('./controllers/albumController');
 app.use('/album', albumRouter);
 
 const ratingRouter = require('./controllers/ratingController');
+const Album = require('./models/Album');
+const Artist = require('./models/Artist');
 app.use('/rating', ratingRouter);
 
 const port = 3000;
